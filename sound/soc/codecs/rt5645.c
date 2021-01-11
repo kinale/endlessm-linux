endlessm/linux

ASoC: rt5645: Invert hp-detect GPIO polarity for EF20EA
This patch add a new DMI to invert the polarity of the hp-detect GPIO
on the ECS EF20EA laptop. This should be properly fixed in the BIOS
possibly using the _DSD property for the RTK2 device:

Name (_DSD, Package () {
    ToUUID("daffd814-6eba-4d8c-8a91-bc9bbf4aa301"),
    Package () {
        Package () {"hp-detect-gpio", Package() {^RTK2, 0, 0, 1 }},
    }
})

Signed-off-by: Carlo Caione <carlo@endlessm.com>

sound/soc/codecs/rt5645.c
@@ -3042,6 +3042,16 @@ int rt5645_set_jack_detect(struct snd_soc_codec *codec,
 }
 EXPORT_SYMBOL_GPL(rt5645_set_jack_detect);
 
+static const struct dmi_system_id invert_hp_detect_dmi_table[] = {
+	{
+		.ident = "EF20EA",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "EF20EA"),
+		}
+	},
+	{ }
+};
+
 static void rt5645_jack_detect_work(struct work_struct *work)
 {
 	struct rt5645_priv *rt5645 =
@@ -3055,6 +3065,8 @@ static void rt5645_jack_detect_work(struct work_struct *work)
 	case 0: /* Not using rt5645 JD */
 		if (rt5645->gpiod_hp_det) {
 			gpio_state = gpiod_get_value(rt5645->gpiod_hp_det);
+			if (dmi_check_system(invert_hp_detect_dmi_table))
+				gpio_state ^= 1;
 			dev_dbg(rt5645->codec->dev, "gpio_state = %d\n",
 				gpio_state);
 			report = rt5645_jack_detect(rt5645->codec, gpio_state);
@@ -3430,7 +3442,6 @@ static struct dmi_system_id dmi_platform_intel_broadwell[] = {
 	},
 	{ }
 };
-
 
 static int rt5645_parse_dt(struct rt5645_priv *rt5645, struct device *dev)
 {

kinale/endlessm-linux

sound/soc/codecs/rt5645.c
@@ -3248,6 +3248,16 @@ int rt5645_set_jack_detect(struct snd_soc_component *component,
 }
 EXPORT_SYMBOL_GPL(rt5645_set_jack_detect);
 
+static const struct dmi_system_id invert_hp_detect_dmi_table[] = {
+	{
+		.ident = "EF20",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "EF20"),
+		}
+	},
+	{ }
+};
+
 static void rt5645_jack_detect_work(struct work_struct *work)
 {
 	struct rt5645_priv *rt5645 =
@@ -3261,6 +3271,8 @@ static void rt5645_jack_detect_work(struct work_struct *work)
 	case 0: /* Not using rt5645 JD */
 		if (rt5645->gpiod_hp_det) {
 			gpio_state = gpiod_get_value(rt5645->gpiod_hp_det);
+			if (dmi_check_system(invert_hp_detect_dmi_table))
+				gpio_state ^= 1;
 			dev_dbg(rt5645->component->dev, "gpio_state = %d\n",
 				gpio_state);
 			report = rt5645_jack_detect(rt5645->component, gpio_state);